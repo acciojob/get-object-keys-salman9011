99038d6c02c6fb3e1c64bce4347ee91f7d0b7ca4
salman <70580281+salman9011@users.noreply.github.com>
2024-01-30 23:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Adding getKeys method to Object prototype
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Creating student object
+const student = {
+    name: "John"
+};
+
+// Test getKeys method
+console.log(student.getKeys()); // Output: ['name']
